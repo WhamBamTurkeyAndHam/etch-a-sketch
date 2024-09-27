e11eb9709341cb748a30dbd7a3fc5e091ef5bd2d
WhamBamTurkeyAndHam <benji.olsen6@gmail.com>
2024-09-27 18:12:01 +1200

    Add color selection drawing
    Using Color Pickr, any color can be picked and drawn with on the grid cells

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This project will test my current skilss, and google searching skills to see if I can create an Etch-A-Sketch that uses a grid to draw on, and the mouse as a the drawing.
+
+Color Pickr By: https://simonwep.github.io/pickr
--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 const mainGridContent = document.querySelector('#js-main-grid-content');
 const retroButton = document.querySelector('#js-retro-button');
-const colorButton = document.querySelector('#js-color-button');
+const randomRainbowButton = document.querySelector('#js-random-rainbow-button');
 const resetButton = document.querySelector('#js-reset-button');
 
 retroButton.addEventListener('click', () => switchToRetro());
-colorButton.addEventListener('click', () => switchToColor());
+randomRainbowButton.addEventListener('click', () => switchToRandomRainbow());
 resetButton.addEventListener('click', () => reset());
 
 let gridCells = 8; //Default
 let gridArea = gridCells * gridCells;
 let retroHandlers = null;
-let randomColorHandlers = null;
+let randomRainbowHandlers = null;
+let randomColorSelectionHandlers = null;
+let colorSelection;
 
 //Creates the grid.
 function createGrid() {
@@ -28,8 +30,8 @@ function createGrid() {
     grid.style.border = gridBorder;
 
     mainGridContent.appendChild(grid);
-  };
-};
+  }
+}
 
 // BLACK DRAWING.
 function retroDrawing() {
@@ -43,8 +45,8 @@ function retroDrawing() {
       event.target.style.backgroundColor = "black";
       event.target.classList.add('gridSquareAnimation');
       event.preventDefault(); // Prevent any unwanted default behavior
-    };
-  };
+    }
+  }
 
   function handleMouseUp() {
     isMouseDown = false;
@@ -54,8 +56,8 @@ function retroDrawing() {
     if (isMouseDown && event.target.classList.contains('gridSquare')) {
       event.target.style.backgroundColor = "black";
       event.target.classList.add('gridSquareAnimation');
-    };
-  };
+    }
+  }
 
   // Handle mouse down event, setting the variable on to draw.
   mainGridContent.addEventListener('mousedown', handleMouseDown);
@@ -67,7 +69,7 @@ function retroDrawing() {
   mainGridContent.querySelectorAll('.gridSquare').forEach(div => div.addEventListener('mouseover', handleMouseOver));
 
   retroHandlers = { handleMouseDown, handleMouseUp, handleMouseOver };
-};
+}
 
 function stopRetroDrawing() {
   if (retroHandlers) {
@@ -78,8 +80,97 @@ function stopRetroDrawing() {
   }
 }
 
-//RANDOM COLOR DRAWING.
-function colorDrawing() {
+//COLOR SELECTION DRAWING.
+
+  // Color Pickr By: https://simonwep.github.io/pickr
+  const pickr = Pickr.create({
+    el: '.colorSelector',
+    theme: 'classic',
+    default: 'rgba(159, 0, 255, 1)',
+    defaultRepresentation: 'RGBA',
+
+    swatches: [
+      'rgba(255, 0, 0, 1)',
+      'rgba(255, 150, 0, 1)',
+      'rgba(252, 255, 0, 1)',
+      'rgba(1, 255, 0, 1)',
+      'rgba(0, 255, 238, 1)',
+      'rgba(0, 21, 255, 1)',
+      'rgba(159, 0, 255, 1)',
+      'rgba(255, 0, 255, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+          hex: true,
+          rgba: true,
+          input: true,
+        }
+    }
+  })
+
+  pickr.on('change', (color) => {
+    colorSelection = color.toRGBA().toString()
+    colorSelectionDrawing(colorSelection);
+    pickr.applyColor(color)
+  })
+
+function colorSelectionDrawing(colorSelection) {
+
+  // Start the variable off.
+  let isMouseDown = false;
+
+  function handleMouseDown(event) {
+    if (event.target.classList.contains('gridSquare')) {
+      isMouseDown = true;
+      // Change color of the square that was clicked to any random color from 0 to 255.
+      event.target.style.backgroundColor = colorSelection;
+      event.target.classList.add('gridSquareAnimation');
+      event.preventDefault(); // Prevent any unwanted default behavior
+    }
+  }
+
+  function handleMouseUp() {
+    isMouseDown = false;
+  }
+
+  function handleMouseOver(event) {
+    if (isMouseDown && event.target.classList.contains('gridSquare')) {
+      this.style.backgroundColor = colorSelection;
+      event.target.classList.add('gridSquareAnimation');
+    }
+  }
+
+  // Handle mouse down event, setting the variable on to draw.
+  mainGridContent.addEventListener('mousedown', handleMouseDown);
+
+  // Handle mouse up event, setting the variable back off from drawing.
+  document.addEventListener('mouseup', handleMouseUp);
+
+  // When the variable is on, add a random background color to each grid one by one, achieving a drawing like feature.
+  mainGridContent.querySelectorAll('.gridSquare').forEach(div => div.addEventListener('mouseover', handleMouseOver));
+
+  randomColorSelectionHandlers = { handleMouseDown, handleMouseUp, handleMouseOver };
+}
+
+function stopColorSelectionDrawing() {
+  if (randomColorSelectionHandlers) {
+    mainGridContent.removeEventListener('mousedown', randomColorSelectionHandlers.handleMouseDown);
+    document.removeEventListener('mouseup', randomColorSelectionHandlers.handleMouseUp);
+    mainGridContent.removeEventListener('mouseover', randomColorSelectionHandlers.handleMouseOver);
+    randomColorSelectionHandlers = null; // Clear randomColorSelectionHandlers after removal
+  }
+}
+
+//RANDOM RAINBOW DRAWING.
+function randomRainbowDrawing() {
 
   // Get random colors and save it into an array.
   function randomRGB() {
@@ -89,7 +180,7 @@ function colorDrawing() {
     let g = randomInteger(255);
     let b = randomInteger(255);
     return [r, g, b];
-  };
+  }
 
   // Start the variable off.
   let isMouseDown = false;
@@ -101,8 +192,8 @@ function colorDrawing() {
       event.target.style.backgroundColor = `rgb(${randomRGB()})`;
       event.target.classList.add('gridSquareAnimation');
       event.preventDefault(); // Prevent any unwanted default behavior
-    };
-  };
+    }
+  }
 
   function handleMouseUp() {
     isMouseDown = false;
@@ -112,8 +203,8 @@ function colorDrawing() {
     if (isMouseDown && event.target.classList.contains('gridSquare')) {
       this.style.backgroundColor = `rgb(${randomRGB()})`;
       event.target.classList.add('gridSquareAnimation');
-    };
-  };
+    }
+  }
 
    // Handle mouse down event, setting the variable on to draw.
   mainGridContent.addEventListener('mousedown', handleMouseDown);
@@ -122,28 +213,33 @@ function colorDrawing() {
   document.addEventListener('mouseup', handleMouseUp);
  
    // When the variable is on, add a random background color to each grid one by one, achieving a drawing like feature.
-  mainGridContent.querySelectorAll('.gridSquare').forEach(div => div.addEventListener('mouseover', handleMouseOver))
+  mainGridContent.querySelectorAll('.gridSquare').forEach(div => div.addEventListener('mouseover', handleMouseOver));
 
-  randomColorHandlers = { handleMouseDown, handleMouseUp, handleMouseOver };
+  randomRainbowHandlers = { handleMouseDown, handleMouseUp, handleMouseOver };
 }
 
-function stopColorDrawing() {
-  if (randomColorHandlers) {
-    mainGridContent.removeEventListener('mousedown', randomColorHandlers.handleMouseDown);
-    document.removeEventListener('mouseup', randomColorHandlers.handleMouseUp);
-    mainGridContent.removeEventListener('mouseover', randomColorHandlers.handleMouseOver);
-    randomColorHandlers = null; // Clear randomColorHandlers after removal
+function stopRandomRainbowDrawing() {
+  if (randomRainbowHandlers) {
+    mainGridContent.removeEventListener('mousedown', randomRainbowHandlers.handleMouseDown);
+    document.removeEventListener('mouseup', randomRainbowHandlers.handleMouseUp);
+    mainGridContent.removeEventListener('mouseover', randomRainbowHandlers.handleMouseOver);
+    randomRainbowHandlers = null; // Clear randomRainbowHandlers after removal
   }
+}
+
+function switchToRetro() {
+  stopRandomRainbowDrawing(); // Remove randomRainbowDrawing eventlisteners
+  retroDrawing(); // Start retroDrawing eventlisteners
 }
 
 function switchToColor() {
   stopRetroDrawing(); // Remove retroDrawing eventlisteners
-  colorDrawing(); // Start colorDrawing eventlisteners
+  colorSelectionDrawing(); // Start color selection drawing
 }
 
-function switchToRetro() {
-  stopColorDrawing(); // Remove colorDrawing eventlisteners
-  retroDrawing(); // Start retroDrawing eventlisteners
+function switchToRandomRainbow() {
+  stopRetroDrawing(); // Remove retroDrawing eventlisteners
+  randomRainbowDrawing(); // Start randomRainbowDrawing eventlisteners
 }
 
 createGrid();
@@ -157,5 +253,5 @@ function reset() {
       div.style.backgroundColor = '';
       div.classList.remove('reset')
     }, { once: true });
-  });
-};
+  })
+}
